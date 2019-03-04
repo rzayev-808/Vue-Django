3d3d5cc147406a82397035d9c1ba3fd0c23e3128
Xoxan808 <xoxan2011@mail.ru>
2019-03-05 02:19:17 +0400

    first commit

--- app/views.py
+++ app/views.py
@@ -7,4 +7,4 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('article_id', 'article_heading', 'article_body')
+    search_fields = ('article_heading', 'article_body', 'city', 'name', 'price')
